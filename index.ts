06fa7f393f0c67083661e2fbea7d3492ff1e3eda
鸽子·湫曗 <autumnsilverleaves@gmail.com>
2020-01-23 11:21:49 +0800

    chore: 下载文件文件名编码

--- index.ts
+++ index.ts
@@ -12,9 +12,28 @@ const csvData = data
   })
   .join("\n");
 
+function encodeRFC5987ValueChars(str) {
+  return (
+    encodeURIComponent(str)
+      // 注意，仅管 RFC3986 保留 "!"，但 RFC5987 并没有
+      // 所以我们并不需要过滤它
+      .replace(/['()]/g, escape) // i.e., %27 %28 %29
+      .replace(/\*/g, "%2A")
+      // 下面的并不是 RFC5987 中 URI 编码必须的
+      // 所以对于 |`^ 这3个字符我们可以稍稍提高一点可读性
+      .replace(/%(?:7C|60|5E)/g, unescape)
+  );
+}
+
+function makeDispositionFileNameField(fileName: string) {
+  return `filename*=UTF-8''${encodeRFC5987ValueChars(fileName)}`;
+}
+
 const server = http.createServer((req, res) => {
+  const fileName = makeDispositionFileNameField("CSV表格.csv");
+
   res.setHeader("Content-Type", "text/csv; charset=utf-8");
-  res.setHeader("Content-Disposition", "attachment; filename=data.csv");
+  res.setHeader("Content-Disposition", `attachment; ${fileName}`);
 
   res.write(csvData);
   res.end();
